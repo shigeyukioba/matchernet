3deb2f2cef77f73977c344ebba7902490dfff86f
uchihashi kenshi <uchi_k@hakuri.local>
2019-11-19 21:37:04 +0900

    #17 test_state の unittest 化

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,22 +1,71 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+
+import unittest
+
 import numpy as np
+from matchernet_py_001 import utils
 from matchernet_py_001.state import StatePlain, StateMuSigma, StateMuSigmaDiag
 
+
+class TestStatePlain(unittest.TestCase):
+
+    def setUp(self):
+        self.n = 4
+        self.s = StatePlain(self.n)
+
+    def test_has_key(self):
+        self.assertTrue(self.s.data.keys() >= {"mu"})
+
+    def test_init_val(self):
+        actual = self.s.data["mu"]
+        expected = utils.zeros((1, self.n))
+        self.assertIsNone(np.testing.assert_array_equal(expected, actual))
+
+
+class TestStateMuSigma(unittest.TestCase):
+
+    def setUp(self):
+        self.n = 4
+        self.s = StateMuSigma(self.n)
+
+    def test_has_key(self):
+        self.assertTrue(self.s.data.keys() >= {"id",
+                                               "time_stamp",
+                                               "mu",
+                                               "Sigma"})
+
+    def test_init_val(self):
+        actual_mu = self.s.data["mu"]
+        expected_mu = utils.zeros((1, self.n))
+        self.assertIsNone(np.testing.assert_array_equal(expected_mu, actual_mu))
+
+        actual_sigma = self.s.data["Sigma"]
+        expected_sigma = np.eye(self.n, dtype=np.float32)
+        self.assertIsNone(np.testing.assert_array_equal(expected_sigma, actual_sigma))
+
+
+class TestStateMuSigmaDiag(unittest.TestCase):
+
+    def setUp(self):
+        self.n = 4
+        self.s = StateMuSigmaDiag(self.n)
+
+    def test_has_key(self):
+        self.assertTrue(self.s.data.keys() >= {"id",
+                                               "time_stamp",
+                                               "mu",
+                                               "sigma"})
+
+    def test_init_val(self):
+        actual_mu = self.s.data["mu"]
+        expected_mu = utils.zeros((1, self.n))
+        self.assertIsNone(np.testing.assert_array_equal(expected_mu, actual_mu))
+
+        actual_sigma = self.s.data["sigma"]
+        expected_sigma = np.diag(np.eye(self.n, dtype=np.float32))
+        self.assertIsNone(np.testing.assert_array_equal(expected_sigma, actual_sigma))
+
 if __name__ == '__main__':
-    print("===Testing StatePlain===")
-    s = StatePlain(4)
-    s.data["mu"][0]=np.array([1,2,3,4],dtype=np.float32)
-    print("mu",s.data["mu"])
-
-    print("===Testing StateMuSigma===")
-    s = StateMuSigma(4)
-    s.data["mu"][0]=np.array([1,2,3,4],dtype=np.float32)
-    s.data["Sigma"]=np.eye(4,dtype=np.float32)
-    print("mu", s.data["mu"])
-    print("Sigma", s.data["Sigma"])
-
-    print("===Testing StateMuSigmaDiag===")
-    s = StateMuSigmaDiag(4)
-    s.data["mu"][0]=np.array([1,2,3,4],dtype=np.float32)
-    s.data["sigma"][0]=np.array([5,5,5,5],dtype=np.float32)
-    print("mu",s.data["mu"])
-    print("sigma",s.data["sigma"])
+    unittest.main()
