4024efc61c067cb32f4c83ed390d62196b11582c
uchihashikenshi <uchihashi.kenshi@gmail.com>
2019-11-22 03:42:22 +0900

    #22 test_observer.py の unittest 化

--- tests/test_observer.py
+++ tests/test_observer.py
@@ -2,44 +2,82 @@ import sys
 import os
 sys.path.append(os.getcwd())
 
+import unittest
+
 import numpy as np
 
 from matchernet_py_001.observer import Observer, ObserverMultiple, missing_handler001
 
-def test_missing_handler001():
-    mu = np.array([1,2,np.nan])
-    Sigma = np.eye(3)
-    mu1, Sigma1 = missing_handler001(mu, Sigma)
-    print("mu",mu)
-    print("Sigma",Sigma)
-    print("mu1",mu1)
-    print("Sigma1",Sigma1)
 
-def test_observer():
-    print("=== Unit test of class Observer ===")
-    dim = 4
-    buffersize = 5
-    x = np.zeros((buffersize,dim),dtype=np.float32)
-    for i in range(buffersize):
-        x[i][0]=i
+class TestMissingHandler001(unittest.TestCase):
 
-    b0 = Observer("b0",x)
-    for i in range(buffersize*2):
-        b0.count_up()
-        print("get_state()=",b0.get_state() )
+    def test_complete(self):
+        test_mu_patterns = [
+            np.array([1, 2, 3]),
+            np.array([1, 2, np.inf])
+        ]
+        for mu in test_mu_patterns:
+            sigma = np.eye(len(mu))
+            with self.subTest():
+                actual_mu, actual_sigma = missing_handler001(mu, sigma)
+                self.assertIsNone(np.testing.assert_array_equal(mu, actual_mu))
+                self.assertIsNone(np.testing.assert_array_equal(sigma, actual_sigma))
 
-    print("=== Unit test of class Observer without BriCA ===")
-    for i in range(buffersize*2):
-        b0(b0.state)
-        print("b0.results[state].data[mu]=",b0.results["state"].data["mu"])
+    def test_missing(self):
+        test_mu_patterns = [
+            np.array([1, 2, np.nan]),
+            np.array([np.nan, np.nan, np.nan])
+        ]
+        expected_mu = [0, 0, 0]
+        for mu in test_mu_patterns:
+            sigma = np.eye(len(mu))
+            with self.subTest():
+                actual_mu, actual_sigma = missing_handler001(mu, sigma)
+                expected_sigma = 1000 * sigma
+                self.assertIsNone(np.testing.assert_array_equal(expected_mu, actual_mu))
+                self.assertIsNone(np.testing.assert_array_equal(expected_sigma, actual_sigma))
 
-    print("=== Unit test of class ObserverMultiple ===")
-    mul = 3
-    b0 = ObserverMultiple("b0",x,mul)
-    for i in range(buffersize*2):
-        b0.count_up()
-        print("get_state()=",b0.get_state() )
+class TestObserver(unittest.TestCase):
+
+    def setUp(self):
+        self.dim = 6
+        self.buffersize = 10
+        self.repeat_num = 3
+        self.x = np.zeros((self.buffersize, self.dim), dtype=np.float32)
+        for i in range(self.buffersize):
+            self.x[i][0] = i
+        self.b = Observer("b0", self.x)
+
+    def test_observer_count_up_with_brica(self):
+        expected = np.zeros(self.dim, dtype=np.float32)
+        for i in range(self.repeat_num):
+            for j in range(self.buffersize):
+                self.b.count_up()
+                actual = self.b.get_state()[0]
+                expected[0] = j
+                self.assertIsNone(np.testing.assert_array_equal(expected, actual))
+
+    def test_observer_count_up(self):
+        expected = np.zeros(self.dim, dtype=np.float32)
+        for i in range(self.repeat_num):
+            for j in range(self.buffersize):
+                self.b(self.b.state)
+                actual = self.b.results["state"].data["mu"][0]
+                expected[0] = j
+                self.assertIsNone(np.testing.assert_array_equal(expected, actual))
+
+    def test_observer_multiple(self):
+        mul = 3
+        b_m = ObserverMultiple("b_m", self.x, mul)
+        for i in range(self.repeat_num):
+            for j in range(self.buffersize):
+                b_m.count_up()
+                actual = b_m.get_state()
+                for k in range(mul):
+                    _ = np.zeros(self.dim, dtype=np.float32)
+                    _[0] = (j + k) % self.buffersize
+                    expected = _ if k == 0 else np.concatenate([expected, _])
+                self.assertIsNone(np.testing.assert_array_equal(expected, actual))
 
 if __name__ == '__main__':
-    test_observer()
-    test_missing_handler001()
+    unittest.main()
