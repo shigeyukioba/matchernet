82894299a67438eb4643ecd16171d3a6251a6dec
uchihashikenshi <uchihashi.kenshi@gmail.com>
2019-11-22 12:40:07 +0900

    #27 __init__.py に情報を追加

--- matchernet_py_001/__init__.py
+++ matchernet_py_001/__init__.py
@@ -6,12 +6,28 @@ matchernet
 matchernet is a
 """
 
-__all__ = ["matchernet","utils","observer","bundlenet_null","bundlenet_ekf","bundlenet_ekf_test","state_space_model_2d","fn"]
+from matchernet_py_001.ekf import *
+from matchernet_py_001.fn import *
+from matchernet_py_001.matchernet *
+from matchernet_py_001.matchernet_null import *
+from matchernet_py_001.observer import *
+from matchernet_py_001.state import *
+from matchernet_py_001.state_space_model_2d import *
+from matchernet_py_001.utils import *
 
-from .matchernet import *
-from .utils import *
-from .state import *
-from .observer import *
-from .matchernet_ekf import *
-from .matchernet_ekf_test import *
-from .app.writing import *
+__copyright__    = 'Copyright (C) 2019 Shigeyuki Oba'
+__version__      = '0.1'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Uchihashi Kenshi'
+__author_email__ = 'uchihashi.kenshi@gmail.com'
+__url__          = 'https://github.com/shigeyukioba/matchernet'
+__all__ = [
+    "ekf",
+    "fn",
+    "matchernet",
+    "matchernet_null",
+    "observer",
+    "state",
+    "state_space_model_2d",
+    "utils"
+]
