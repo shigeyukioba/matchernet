0ff38dbcb75f199a95a6ebd1092310cbaa979227
kosuke miyoshi <miyoshi@narr.jp>
2019-11-22 18:13:37 +0900

    add AimGIF writer for result sharing in Github issue #28

--- matchernet/util/recording.py
+++ matchernet/util/recording.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import os
+import shutil
 import cv2
+import subprocess
 
 
 class MovieWriter(object):
@@ -26,3 +29,37 @@ class MovieWriter(object):
     def close(self):
         self.vout.release()
         self.vout = None
+
+
+class AnimGIFWriter(object):
+    """ Anim GIF output utility class. """
+    def __init__(self, file_name, fps, tmp_dir_name="tmp_gif"):
+        """
+        frame_size is (w, h)
+        """
+        self.file_name = file_name
+        self.fps = fps
+        self.tmp_dir_name = tmp_dir_name
+        self.image_index = 0
+
+        if os.path.exists(self.tmp_dir_name):
+            shutil.rmtree(self.tmp_dir_name)
+        os.mkdir(self.tmp_dir_name)
+
+    def add_frame(self, frame):
+        """
+        frame shape is (h, w, 3), dtype is np.uint8
+        """
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        file_path = os.path.join(self.tmp_dir_name, "frame{0:03d}.png".format(self.image_index))
+        cv2.imwrite(file_path, frame)
+        self.image_index += 1
+        
+    def close(self):
+        delay = int(100 / self.fps)
+        print("delay = {}".format(delay))
+        command = "convert -delay {} {}/*.png {}".format(delay,
+                                                         self.tmp_dir_name,
+                                                         self.file_name)
+        subprocess.call(command.split())
+        shutil.rmtree(self.tmp_dir_name)
