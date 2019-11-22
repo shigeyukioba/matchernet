90c7cacbb5d061b5c2cfaed87e694e6b62dc7254
uchihashikenshi <uchihashi.kenshi@gmail.com>
2019-11-22 12:40:39 +0900

    #27 setup.py を追加

---
+++ setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup
+from codecs import open
+from os import path
+import re
+
+package_name = "matchernet_py_001"
+
+root_dir = path.abspath(path.dirname(__file__))
+
+def _requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
+
+
+def _test_requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'test-requirements.txt')).readlines()]
+
+with open(path.join(root_dir, package_name, '__init__.py')) as f:
+    init_text = f.read()
+    version = re.search(r'__version__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    license = re.search(r'__license__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    author = re.search(r'__author__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    author_email = re.search(r'__author_email__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    url = re.search(r'__url__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+
+assert version
+assert license
+assert author
+assert author_email
+assert url
+
+with open('readme.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name=package_name,
+    packages=[package_name],
+
+    version=version,
+
+    license=license,
+
+    install_requires=_requirements(),
+    tests_require=_test_requirements(),
+
+    author=author,
+    author_email=author_email,
+
+    url=url,
+
+    description='',
+    long_description=long_description,
+    keywords='kalman filter, neural net',
+
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
