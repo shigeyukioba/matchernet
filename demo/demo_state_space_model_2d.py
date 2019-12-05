32cb8d54e1f66716669a0afff94035b3f5f29148
uchihashikenshi <uchihashi.kenshi@gmail.com>
2019-12-06 07:07:05 +0900

    #21 状態空間モデルに外部パラメータを渡せるようにしたことへのデモの対応

--- demo/demo_state_space_model_2d.py
+++ demo/demo_state_space_model_2d.py
@@ -2,29 +2,41 @@ import logging
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matchernet_py_001 import fn
 from matchernet_py_001.state_space_model_2d import StateSpaceModel2Dim
+from matchernet_py_001 import utils
 
 logger = logging.getLogger(__name__)
-def demo_state_space_model_2dim(numSteps):
-    dt = 0.1
-    ssm = StateSpaceModel2Dim(dt)
-    x_series, y_series = ssm.simulation(dt, numSteps)
+def demo_state_space_model_2dim(n_dim, A, g, sigma_w, sigma_z, x, y, n_steps, dt):
+    ssm = StateSpaceModel2Dim(n_dim, A, g, sigma_w, sigma_z, x, y)
+    x_series, y_series = ssm.simulation(n_steps, dt)
     logger.info('x: {}'.format(x_series))
     logger.info('y: {}'.format(y_series))
 
     plt.subplot(211)
-    timestamp=np.array(range(0,numSteps))
-    plt.plot(timestamp,x_series[:,0])
-    plt.scatter(timestamp,y_series[:,0],s=1)
+    timestamp=np.array(range(0, n_steps))
+    plt.plot(timestamp, x_series[:, 0])
+    plt.scatter(timestamp, y_series[:, 0], s=1)
     plt.title("test_state_space_model_2Dim")
     plt.ylabel("X")
     plt.subplot(212)
-    plt.plot(timestamp,x_series[:,1])
-    plt.scatter(timestamp,y_series[:,1],s=1)
+    plt.plot(timestamp, x_series[:, 1])
+    plt.scatter(timestamp, y_series[:, 1], s=1)
     plt.ylabel("Y")
     plt.show()
 
 if __name__ == '__main__':
     formatter = '[%(asctime)s] %(module)s.%(funcName)s %(levelname)s -> %(message)s'
     logging.basicConfig(level=logging.INFO, format=formatter)
-    demo_state_space_model_2dim(500)
+
+    n_steps = 500
+    n_dim = 2
+    dt = 0.1
+    A = np.array([[-0.1, 2], [-2, -0.1]], dtype=np.float32)
+    g = fn.LinearFn(n_dim, n_dim)
+    sigma_w = 0.1
+    sigma_z = 0.1
+    x = np.array([0, 0], dtype=np.float32)
+    y = utils.zeros((1, 2))
+
+    demo_state_space_model_2dim(n_dim, A, g, sigma_w, sigma_z, x, y, n_steps, dt)
