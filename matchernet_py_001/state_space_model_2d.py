af7015b96fea21edd3a4d7f97175891c61c2e98f
uchihashikenshi <uchihashi.kenshi@gmail.com>
2019-12-06 07:03:35 +0900

    #21 状態空間モデルのパラメータを外部から渡せるように

--- matchernet_py_001/state_space_model_2d.py
+++ matchernet_py_001/state_space_model_2d.py
@@ -4,39 +4,35 @@ from matchernet_py_001 import fn
 from matchernet_py_001 import utils
 
 class StateSpaceModel2Dim(object):
-    def __init__(self,dt):
-        n=2
-        self.n=n
-        #self.w=N(0,sigma)
-        self.A=np.array([[-0.1,2],[-2,-0.1]],dtype=np.float32)
-        #self.f=utils.MyLinear(n,n,self.A);
-        self.F=utils.calc_matrix_F(self.A,dt)
-        self.g=fn.LinearFn(n,n)
-        self.g.params["A"] = self.A
-        self.sigma_w=0
-        self.sigma_z=0.1
-        self.x=np.array([0,1],dtype=np.float32)
-        self.y=utils.zeros((1,2))
 
-    def simulation(self,dt, numSteps):
-        x0=self.x
-        for i in range(numSteps):
-            (x, y)=self._step(dt)
-            if i==0:
-                self.xrec = x
-                self.yrec = y
+    def __init__(self, n_dim, A, g, sigma_w, sigma_z, x, y):
+        if n_dim != 2:
+            raise NotImplementedError
+        self.n_dim = n_dim
+        self.A = A
+        self.g = g
+        self.sigma_w = sigma_w
+        self.sigma_z = sigma_z
+        self.x = x
+        self.y = y
+
+    def simulation(self, n_steps, dt):
+        F = utils.calc_matrix_F(self.A, dt)
+        for i in range(n_steps):
+            (x, y) = self._step(F)
+            if i == 0:
+                x_series = x
+                y_series = y
             else:
-                self.xrec=np.concatenate((self.xrec,x),axis=0)
-                self.yrec=np.concatenate((self.yrec,y),axis=0)
+                x_series = np.concatenate((x_series, x), axis=0)
+                y_series = np.concatenate((y_series, y), axis=0)
 
-        return (self.xrec, self.yrec)
+        return x_series, y_series
 
-    def _step(self, dt):
-        F=self.F
-        self.w = self.sigma_w * np.random.standard_normal(size=(1,self.n))
-        #pdb.set_trace()
-        self.x = np.dot( self.x, F )+self.w
-        self.z = self.sigma_z * np.random.standard_normal(size=(1,self.n))
-        self.y = self.x+self.z
-        #Sigma = dt * Q + np.dot( np.dot( F.T,  Sigma), F )
-        return (self.x, self.y)
+    def _step(self, F):
+        self.w = self.sigma_w * np.random.standard_normal(size=(1, self.n_dim))
+        self.x = np.dot(self.x, F) + self.w
+        self.z = self.sigma_z * np.random.standard_normal(size=(1, self.n_dim))
+        self.y = self.x + self.z
+
+        return self.x, self.y
