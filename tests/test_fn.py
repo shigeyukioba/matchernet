048d36a8e34ecd0b8e02663482a621b7d1b8558b
uchihashikenshi <uchihashi.kenshi@gmail.com>
2019-12-07 11:01:33 +0900

    #18 test_fn.py の unittest 化

--- tests/test_fn.py
+++ tests/test_fn.py
@@ -1,47 +1,75 @@
+import unittest
+
 import numpy as np
 from matchernet_py_001.fn import LinearFn, LinearFnXU
 
-def test_LinearFn():
-    print("=== testLinearFn() ===")
-    matA = np.array([[5,1,1],[1,2,3]],dtype=np.float32)
-    x = np.array([[3,1]],dtype=np.float32)
-    y = np.dot(x,matA)
-    f = LinearFn(3,2)
-    f.params["A"] = matA
-    yout = f.value(x)
-    print("matA:",matA)
-    print("x:",x)
-    print("y:",y)
-    print("f(x):",yout)
-    print("f_x(x):",f.x(x))
-    print("f_xx(x):",f.xx(x))
 
-def test_LinearFnXU():
-    print("=== testLinearFnXU ===")
-    matA = np.array([[5,1,1],[1,2,3]],dtype=np.float32)
-    matB = np.array([[1,0,0]],dtype=np.float32)
-    x = np.array([[3,1]],dtype=np.float32)
-    u = np.array([[1]])
-    y = np.dot(x,matA) + np.dot(u,matB)
-    f = LinearFnXU(3,1,2)
-    f.params["A"] = matA
-    f.params["B"] = matB
-    yout = f.value(x,u)
-    print("matA:",matA)
-    print("matB:",matB)
-    print("x:",x)
-    print("u:",u)
-    print("y:",y)
-    print("f(x,u):",yout)
-    print("f_x(x,u):",f.x(x,u))
-    print("f_xx(x,u):",f.xx(x,u))
-    print("f_u(x,u):",f.u(x,u))
-    print("f_xu(x,u):",f.xu(x,u))
-    print("f_uu(x,u):",f.uu(x,u))
+class TestLinearFn(unittest.TestCase):
 
-def test_all():
-    test_LinearFn()
-    test_LinearFnXU()
+    def setUp(self):
+        self.x = np.array([10, 20, 30], dtype=np.float32)
+        self.test_A_patterns = [
+            np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float32),
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=np.float32),
+            np.array([[1, 2, np.inf]], dtype=np.float32)
+        ]
+
+    def test_linear_dot(self):
+        for A in self.test_A_patterns:
+            f = LinearFn(A)
+            expected_dot = np.dot(A, self.x)
+            with self.subTest():
+                actual_dot = f.value(self.x)
+                self.assertIsNone(np.testing.assert_array_equal(expected_dot, actual_dot))
+
+    def test_linear_dx(self):
+        for A in self.test_A_patterns:
+            f = LinearFn(A)
+            expected_dx = A
+            with self.subTest():
+                actual_dx = f.dx(self.x)
+                self.assertIsNone(np.testing.assert_array_equal(expected_dx, actual_dx))
+
+
+class TestLinearFnXU(unittest.TestCase):
+
+    def setUp(self):
+        self.x = np.array([10, 20, 30], dtype=np.float32)
+        self.u = np.array([5, 15, 25], dtype=np.float32)
+        self.test_A_patterns = [
+            np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float32),
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=np.float32),
+            np.array([[1, 2, np.inf]], dtype=np.float32)
+        ]
+        self.test_B_patterns = [
+            np.array([[6, 5, 4], [3, 2, 1]], dtype=np.float32),
+            np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]], dtype=np.float32),
+            np.array([[np.inf, 2, 3]], dtype=np.float32)
+        ]
+
+    def test_linear_XU_dot(self):
+        for A, B in zip(self.test_A_patterns, self.test_B_patterns):
+            f = LinearFnXU(A, B)
+            expected_dot = np.dot(A, self.x) + np.dot(B, self.u)
+            with self.subTest():
+                actual_dot = f.value(self.x, self.u)
+                self.assertIsNone(np.testing.assert_array_equal(expected_dot, actual_dot))
+
+    def test_linear_XU_dx(self):
+        for A, B in zip(self.test_A_patterns, self.test_B_patterns):
+            f = LinearFnXU(A, B)
+            expected_dx = A
+            with self.subTest():
+                actual_dx = f.dx(self.x, self.u)
+                self.assertIsNone(np.testing.assert_array_equal(expected_dx, actual_dx))
+
+    def test_linear_XU_du(self):
+        for A, B in zip(self.test_A_patterns, self.test_B_patterns):
+            f = LinearFnXU(A, B)
+            expected_du = B
+            with self.subTest():
+                actual_du = f.du(self.x, self.u)
+                self.assertIsNone(np.testing.assert_array_equal(expected_du, actual_du))
 
 if __name__ == '__main__':
-    test_all()
+    unittest.main()
