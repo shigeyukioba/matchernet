7d9272701677b9d613ad3c555fd94e84dea0c7b9
uchihashikenshi <uchihashi.kenshi@gmail.com>
2020-02-08 01:03:51 +0900

    #54 setup の微修正

--- setup.py
+++ setup.py
@@ -32,30 +32,20 @@ with open('readme.md', encoding='utf-8') as f:
 setup(
     name=package_name,
     packages=find_packages(exclude=('tests', )),
-
-    version="0.0.3",
-
+    version=version,
     license=license,
-
     install_requires=_requirements(),
-
     author=author,
     author_email=author_email,
-
     url=url,
-
     description='',
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords='kalman filter, neural net',
-
     classifiers=[
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
