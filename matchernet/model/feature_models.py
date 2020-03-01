4f6b8ec0fb6768781b74dfb95b49599052de51e8
uchihashikenshi <uchihashi.kenshi@gmail.com>
2020-03-01 22:17:44 +0900

    #50 ドキュメントの英語化

--- matchernet/model/feature_models.py
+++ matchernet/model/feature_models.py
@@ -3,30 +3,33 @@ import keras.layers as layer
 
 
 def image_feature(inputs, output_size):
-    """xception による画像特徴量抽出と固定長への変換
+    """Image feature extraction by xception and conversion to fixed length
 
-    keras に実装されている、ImageNet で学習した重みを利用可能な Xception V1 のモデル
-    (keras.applications.Xception) を使用する。
-    このモデルの ImageNet での精度は、
-        top-1 validation accuracy: 0.790
-        top-5 validation accuracy: 0.945
-    である。
-    この際、include_top=False とすることで、出力の全結合層を外している。こうすることで、
-    特徴抽出の部分のみ利用可能になる。
-    この特徴量を1次元化し全結合層を通して output_size の出力を得る。
-    特徴量の1次元化によく使われるのは flatten で、これは全結合層に入力する前に (x, y, channels)
-    次元の特徴量を (x*y*channels) の1次元に変換するものだが、GlobalAveragePooling2D では
-    (x, y) 次元の特徴マップを1次元に average pooling し、(channels) の1次元特徴に変換する
-    方式を採用した。これは、パラメータ数を抑制しつつ精度も出せることが知られている。また、
-    これにより、入力サイズが任意になっている。
-    Dense で (batch_size, channels) から (batch_size, output_size) の特徴量を出力する。
+    Use the Xception V1 model (keras.applications.Xception) that can use
+    the weights learned in ImageNet, implemented in keras.
+    The accuracy of this model on ImageNet is
+        top-1 validation accuracy: 0.790
+        top-5 validation accuracy: 0.945
+    At this time, by setting include_top = False, the entire coupling layer
+    of the output is removed. In this way, only the feature extraction part
+    can be used.
+    This feature is made one-dimensional, and the output of output_size is
+    obtained through the fully connected layer.
+    A common use for flattening features is flatten, which converts
+    (x, y, channels) dimensions to one (x * y * channels) dimension before
+    entering the fully connected layer. However, GlobalAveragePooling2D adopts
+    a method of performing average pooling of (x, y)-dimensional feature maps
+    into one dimension and converting them into (channels) one-dimensional features.
+    It is known that the accuracy can be improved while suppressing the number of
+    parameters. In addition, this makes the input size arbitrary.
+    Dense outputs the (batch_size, channels) to (batch_size, output_size) features.
 
     Args:
-        inputs(numpy.array): (x, y, channels), 入力画像特徴
-        output_size(int): 出力する特徴量のサイズ
+        inputs(numpy.array): (x, y, channels), Input image features
+        output_size(int): Output feature size
 
     Returns:
-        x: (batch_size, output_size), 画像特徴量
+        x: (batch_size, output_size), Image features
     """
     inception_feature = keras.applications.Xception(
         input_tensor=inputs,
